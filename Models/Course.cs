8e92bc233ee28f9236e904544e8c84bd59b37080
lucashenryck <lucashenryck071@gmail.com>
2022-03-29 21:53:37 -0400

    mock bd

---
+++ Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace aulaweb.Models;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Image { get; set; }
+
+}
